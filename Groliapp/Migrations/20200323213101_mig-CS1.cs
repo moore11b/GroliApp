8711a9bd684fe61c41cc1e6105b2f5e9a6c2777e
Cory Moore <moore11b@gmail.com>
2020-03-23 17:31:36 -0400

    Connection String
    Correction with the DB string name.

---
+++ Groliapp/Migrations/20200323213101_mig-CS1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Groliapp.Migrations
+{
+    public partial class migCS1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
